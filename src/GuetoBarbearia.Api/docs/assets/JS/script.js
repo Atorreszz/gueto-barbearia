6865956e5dafc1ad8750a5748bf947744f1b0163
Arthur Torres <arthurtorresat39@gmail.com>
2026-09-07 08:53:05 -0300

    Adiciona lógica de seleção de data e serviços no JavaScript

--- src/GuetoBarbearia.Api/docs/assets/JS/script.js
+++ src/GuetoBarbearia.Api/docs/assets/JS/script.js
@@ -1,1 +1,39 @@
-console.log("Javascript e Gueto Conectados")
+const campoData = document.querySelector("#data");
+const campoHorario = document.querySelector("#horario");
+
+const hoje = new Date();
+
+const ano = hoje.getFullYear();
+const mes = String(hoje.getMonth() + 1).padStart(2, "0");
+const dia = String(hoje.getDate()).padStart(2, "0");
+
+const dataMinima = `${ano}-${mes}-${dia}`;
+
+campoData.min = dataMinima;
+
+campoData.addEventListener("change", () => {
+    const possuiDataSelecionada = campoData.value !== "";
+
+    campoHorario.disabled = !possuiDataSelecionada;
+
+    if (!possuiDataSelecionada) {
+        campoHorario.value = "";
+    }
+});
+const servicos = [
+    {
+        id: "twist",
+        nome: "Twist",
+        preco: 120.00,
+        duracaoMinutos: 140
+    },
+    {
+        id: "barrel",
+        nome: "Barrel Twist",
+        preco: 150,
+        duracaoMinutos: 150
+    }
+];
+console.table(servicos);
+console.log(servicos[0].nome);
+console.log(servicos[1].preco);
